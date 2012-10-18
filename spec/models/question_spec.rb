6d14f198130e139d0f93faf60b055fc4b80c9793
Mike Patterson <mikepatterson77@gmail.com>
2012-10-17 21:16:38 -0400

    tests now work with ruby Itest

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require_relative '../spec_helper'
 
 describe Question do  
   it "requires a question" do
--- spec/requests/questions_spec.rb
+++ spec/requests/questions_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require_relative '../spec_helper'
 
 describe "Questions" do
   describe "GET /questions" do
